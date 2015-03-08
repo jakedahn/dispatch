df1bcb4a38773f6b24d66d04a4d7bb3d66100a87
jakedahn <jake@markupisart.com>
2015-03-07 17:24:21 -0800

    getting started on the fetcher component

---
+++ fetcher/fetcher.go
@@ -0,0 +1,128 @@
+// Copyright 2015 Jake Dahn
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fetcher
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"strings"
+
+	git "gopkg.in/libgit2/git2go.v22"
+	yaml "gopkg.in/yaml.v2"
+)
+
+type Repo struct {
+	Name         string
+	GitUrl       string
+	GitRef       string
+	GitRepo      *git.Repository
+	CheckoutPath string
+}
+
+type DispatchFile struct {
+	BuildSteps []string               `yaml:"build"`
+	Arguments  []DispatchfileArgument `yaml:"arguments"`
+}
+
+type DispatchfileArgument struct {
+	Key      string
+	Type     string
+	Presence string
+}
+
+type DispatchRequest struct {
+	GitUrl    string              `json:"repo_url"`
+	GitRef    string              `json:"ref"`
+	Arguments []map[string]string `json:"arguments"`
+}
+
+func NewRepo(gitUrl, gitRef string) *Repo {
+	repoUrlParts := strings.Split(gitUrl, "/")
+	repoUrlParts = strings.Split(repoUrlParts[len(repoUrlParts)-1], ".")
+	name := repoUrlParts[len(repoUrlParts)-2]
+
+	repo := &Repo{
+		GitUrl: gitUrl,
+		GitRef: gitRef,
+		Name:   name,
+	}
+	return repo
+}
+
+func (r *Repo) Init() error {
+	if r.GitUrl == "" || r.GitRef == "" {
+		return errors.New("r.GitUrl or r.Gitref are undefined")
+	}
+
+	tmpDir := "/tmp/dispatch-tmp"
+	checkoutPath, err := ioutil.TempDir(tmpDir, r.Name)
+	if err != nil {
+		return fmt.Errorf("Unable to create directory: %s", err)
+	}
+
+	r.CheckoutPath = checkoutPath
+
+	cloneOpts := &git.CloneOptions{}
+	gitRepo, err := git.Clone(r.GitUrl, checkoutPath, cloneOpts)
+	if err != nil {
+		return fmt.Errorf("Unable to clone the repo: %s", err)
+	}
+	r.GitRepo = gitRepo
+
+	return nil
+}
+
+func (r *Repo) Checkout() error {
+	if r.GitUrl == "" || r.GitRef == "" || r.CheckoutPath == "" {
+		return errors.New("r.GitUrl or r.Gitref are undefined or r.CheckoutPath")
+	}
+
+	sha, err := git.NewOid(r.GitRef)
+	if err != nil {
+		return err
+	}
+
+	commit, err := r.GitRepo.LookupCommit(sha)
+	if err != nil {
+		return err
+	}
+
+	err = r.GitRepo.SetHeadDetached(commit.Id(), nil, "")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewDispatchRequest(requestJson []byte) (*DispatchRequest, error) {
+	dispatchRequest := &DispatchRequest{}
+	err := json.Unmarshal(requestJson, dispatchRequest)
+	if err != nil {
+		return nil, err
+	}
+	return dispatchRequest, nil
+}
+
+func ParseDispatchFile(dispatchFileContent []byte) (*DispatchFile, error) {
+	df := &DispatchFile{}
+	err := yaml.Unmarshal(dispatchFileContent, df)
+	if err != nil {
+		return nil, err
+	}
+	return df, nil
+}
--- fetcher/fetcher_test.go
+++ fetcher/fetcher_test.go
@@ -24,13 +24,82 @@ func TestParseAppMetric(t *testing.T) {
 
 	Convey("Given a valid Dispatch Request, the Fetcher should be able", t, func() {
 
-		Convey("to fetch the git repository", nil)
+		jsonRequest := `{
+            "repo_url": "https://github.com/jakedahn/echo.git",
+            "ref": "9284929047170968a2f5ab92968c3abac9242cc3",
+            "arguments": [{"GOECHO": "wheeeeeeee"}]
+        }`
 
-		Convey("to read the Dispatchfile", nil)
+		dispatchRequest, err := NewDispatchRequest([]byte(jsonRequest))
+		So(err, ShouldBeNil)
+		So(dispatchRequest.GitUrl, ShouldEqual, "https://github.com/jakedahn/echo.git")
+		So(dispatchRequest.GitRef, ShouldEqual, "9284929047170968a2f5ab92968c3abac9242cc3")
 
-		Convey("to parse the build steps", nil)
+		// fixme: I don't feel good about this data structure, feels weird
+		for key, value := range dispatchRequest.Arguments[0] {
+			So(key, ShouldEqual, "GOECHO")
+			So(value, ShouldEqual, "wheeeeeeee")
+		}
 
-		Convey("to parse the argument specifications", nil)
+		Convey("to parse the contents of a Dispatchfile", func() {
+			dfRaw := `
+                build:
+                  - CGO_ENABLED=0 go build -o ./bin/echo -a main.go
+                  - docker build .
 
+                arguments:
+                  - {key: GOECHO, type: string, presence: required}`
+
+			df, err := ParseDispatchFile([]byte(dfRaw))
+			So(err, ShouldBeNil)
+
+			Convey("specifically the build steps", func() {
+				So(df.BuildSteps[0], ShouldEqual, "CGO_ENABLED=0 go build -o ./bin/echo -a main.go")
+				So(df.BuildSteps[1], ShouldEqual, "docker build .")
+			})
+
+			Convey("specifically the arguments", func() {
+				So(df.Arguments[0].Key, ShouldEqual, "GOECHO")
+				So(df.Arguments[0].Type, ShouldEqual, "string")
+				So(df.Arguments[0].Presence, ShouldEqual, "required")
+			})
+		})
+	})
+}
+
+func TestDispatchFetcherIntegration(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode.")
+	}
+
+	Convey("Gifven a valid Dispatch Request, the Fetcher should be able", t, func() {
+
+		jsonRequest := `{
+            "repo_url": "https://github.com/jakedahn/echo.git",
+            "ref": "9284929047170968a2f5ab92968c3abac9242cc3",
+            "arguments": [{"GOECHO": "wheeeeeeee"}]
+        }`
+
+		dispatchRequest, err := NewDispatchRequest([]byte(jsonRequest))
+		So(err, ShouldBeNil)
+
+		Convey("to fetch the git repository", func() {
+			repo := NewRepo(dispatchRequest.GitUrl, dispatchRequest.GitRef)
+			So(repo.GitUrl, ShouldEqual, dispatchRequest.GitUrl)
+			So(repo.GitRef, ShouldEqual, dispatchRequest.GitRef)
+			So(repo.CheckoutPath, ShouldEqual, "")
+
+			err := repo.Init()
+			So(err, ShouldBeNil)
+			So(repo.CheckoutPath, ShouldNotEqual, "")
+
+			err = repo.Checkout()
+			So(err, ShouldBeNil)
+			head, err := repo.GitRepo.Head()
+			So(err, ShouldBeNil)
+			headCommit, err := repo.GitRepo.LookupCommit(head.Target())
+			So(err, ShouldBeNil)
+			So(headCommit.Id().String(), ShouldEqual, "9284929047170968a2f5ab92968c3abac9242cc3")
+		})
 	})
 }
